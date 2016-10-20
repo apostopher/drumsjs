c231278c76879876939c1f9c07114cd844a4dfd6
Josh Hopkins <josh@hoppie.co>
2016-10-20 20:53:47 +0800

    Functional js
    Lost most of this as it didn’t save

---
+++ js/02 Intro to Functional js/Functional Javascript.js
@@ -0,0 +1,18 @@
+/*
+map -> [1,2,3,4] - (fx) -> [2, 3, 4, 5]
+
+fx = add 1
+dx getStockPrices
+
+reduce -> [1,2,3,4] -> (fx) -> 10
+
+filter -> [1,2,3,4] (fx) -> [3,4]
+
+fx = num > 2
+
+
+const kids
+
+function gifts (kids) [
+
+]
